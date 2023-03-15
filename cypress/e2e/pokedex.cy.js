0e21f5f8cd2506ba9dfe3be0808c7e41a8951031
kaifJ <kaif.jamadar.in@gmail.com>
2023-03-15 21:29:45 +0200

    add cypress test

---
+++ cypress/e2e/pokedex.cy.js
@@ -0,0 +1,14 @@
+describe('pokedex', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3001')
+  })
+  it('front page can be opened', function () {
+    cy.contains('ivysaur')
+    cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  })
+
+  it('navigate to ivysaur Pokemon page', function () {
+    cy.contains('ivysaur').click()
+    cy.contains('chlorophyll')
+  })
+})
